07793903670875ef48b7a45c16df82c1ffcefd1a
Sorrawit <sorrawit456789@gmail.com>
2022-05-09 15:43:23 +0700

    W2-03 Header and Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import Main from './components/Main.component';
+import './css/App.css';
 
 export default class App extends Component {
 
--- src/components/Main.component.jsx
+++ src/components/Main.component.jsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import { Navbar, NavbarBrand } from 'reactstrap'
 import Menu from './Menu.component'
 import { DISHES } from '../shared/dishes'
+import Header from './Header.component'
+import Footer from './Footer.component'
 
 export default class Main extends Component {
   constructor(props) {
@@ -22,7 +24,9 @@ export default class Main extends Component {
             <NavbarBrand href='/'>Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
+        <Header />
         <Menu dishes={this.state.dishes} selectedDish={this.state.selectedDish} onClick={(dishId) => this.onDishSelect(dishId)} />
+        <Footer />
       </div>
     )
   }
